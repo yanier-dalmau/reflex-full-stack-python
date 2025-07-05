46f87db83f731d895ccf3d4ab22ab2f8d73d2962
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-07-04 23:25:07 -0400

    001 - Dynamic Content and on_click Events
    feat(ui+state): Mejorar la gestión del estado y la interfaz de usuario
    
    - full_stack_python/full_stack_python.py:
      * Agregar atributo 'label' y 'original_label' a la clase State
      * Implementar método 'change_label' para alternar etiquetas
      * Actualizar encabezado para mostrar la etiqueta del estado
      * Añadir botón para ejecutar la función 'change_label'
    
    Nota: Asegúrate de que la lógica de cambio de etiqueta funcione como se espera.

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -7,21 +7,27 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
+    label = "This is my label"
+    original_label = "This is my label"
 
-    ...
-
+    def change_label(self):
+        if self.label == "Cool label.":
+            self.label = self.original_label
+        else:   
+            self.label = "Cool label."
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="bottom-left"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, " Welcome to Reflex!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
+            rx.button("Do something!", on_click=State.change_label),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
