3294c305c3146224274fe48ff15816e2a920d9c7
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-07-04 23:47:43 -0400

    002 - HTML Inputs for Dynamic Content Changes
    refactor(ui+state): Simplificar gestión de estado y mejorar entrada de usuario
    
    - full_stack_python/full_stack_python.py:
      * Actualizar etiqueta de estado a "Welcome to Reflex!"
      * Eliminar método 'change_label' y lógica asociada
      * Añadir método 'handle_title_input_change' para actualizar la etiqueta desde la entrada del usuario
      * Incluir campo de entrada para que el usuario modifique la etiqueta
    
    Nota: La interfaz ahora permite al usuario personalizar la etiqueta directamente.

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -7,28 +7,25 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
-    label = "This is my label"
-    original_label = "This is my label"
-
-    def change_label(self):
-        if self.label == "Cool label.":
-            self.label = self.original_label
-        else:   
-            self.label = "Cool label."
+    label = "Welcome to Reflex!"
+    
+    def handle_title_input_change(self, val):
+        self.label = val
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="bottom-left"),
         rx.vstack(
-            rx.heading(State.label, " Welcome to Reflex!", size="9"),
+            rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
-                on_click=State.change_label,
                 size="5",
             ),
-            # rx.button("Do something!", on_click=State.change_label),
+            rx.input(
+                on_change=State.handle_title_input_change
+            ),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
