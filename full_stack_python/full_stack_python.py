1d4f438c2ab2835e85f82ef5d2727c4e6f007f04
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-07-28 12:15:56 -0400

    007 - Full Width Navbar and Content
    refactor(ui): Mejorar la estructura del componente de la página de inicio
    
    - full_stack_python/full_stack_python.py:
      * Extraer la definición del contenido de la página de inicio a una variable 'my_child' para mayor claridad
      * Añadir propiedades de alineación y un ID al componente 'my_child' para facilitar el estilo y la manipulación
    
    Nota: Esta reorganización mejora la legibilidad del código y permite un mejor control sobre el contenido de la página de inicio.

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -18,29 +18,32 @@ class State(rx.State):
     
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return base_page(
-        rx.vstack(
-            rx.heading(State.label, size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.input(
-                default_value=State.label,
-                on_click=State.did_click,
-                on_change=State.handle_title_input_change
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        )
+    my_child = rx.vstack(
+        rx.heading(State.label, size="9"),
+        rx.text(
+            "Get started by editing ",
+            rx.code(f"{config.app_name}/{config.app_name}.py"),
+            size="5",
+        ),
+        rx.input(
+            default_value=State.label,
+            on_click=State.did_click,
+            on_change=State.handle_title_input_change
+        ),
+        rx.link(
+            rx.button("Check out our docs!"),
+            href="https://reflex.dev/docs/getting-started/introduction/",
+            is_external=True,
+        ),
+        spacing="5",
+        justify="center",
+        align="center",
+        text_align="center",
+        min_height="85vh",
+        id='my-child'
     )
+    
+    return base_page(my_child)
 
 
 app = rx.App()
