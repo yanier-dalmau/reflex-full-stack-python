c95e57914e690d7c8bd409badd73dc675a9fea2e
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-07-28 11:27:05 -0400

    006 - Idenitfying Rendered Components in the Browser
    feat(ui): Añadir identificadores a componentes para mejor accesibilidad
    
    - full_stack_python/ui/base.py:
      * Asignar ID "my-light-color-btn" al botón de modo de color
      * Asignar ID "my-base-container" al contenedor principal de la página base
    
    Nota: Los identificadores mejoran la accesibilidad y permiten una mejor manipulación del DOM.

--- full_stack_python/ui/base.py
+++ full_stack_python/ui/base.py
@@ -17,5 +17,6 @@ def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Com
         navbar(),
         child,
         rx.logo(),
-        rx.color_mode.button(position="bottom-left"),
+        rx.color_mode.button(position="bottom-left", id="my-light-color-btn"),
+        id="my-base-container"
     )
